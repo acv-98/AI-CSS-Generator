4a81553fb56671264864c12a0357cc405e1b4d34
acv-98 <andrei.vanvu@outlook.com>
2020-10-02 09:49:12 +0100

    Updates

---
+++ precache-manifest.f23b7b9e2f4c1c7026d5e8a850922edd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b42f930edd3040fcd8d0c8ad898508d",
+    "url": "/AI-CSS-Generator/index.html"
+  },
+  {
+    "revision": "a02ba66086056a1d12f6",
+    "url": "/AI-CSS-Generator/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "408d41df89a28207c5fb",
+    "url": "/AI-CSS-Generator/static/css/main.f3b3cb8c.chunk.css"
+  },
+  {
+    "revision": "a02ba66086056a1d12f6",
+    "url": "/AI-CSS-Generator/static/js/2.302faa84.chunk.js"
+  },
+  {
+    "revision": "2873965d810bb5abb693cdedb5ebc7b0",
+    "url": "/AI-CSS-Generator/static/js/2.302faa84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "408d41df89a28207c5fb",
+    "url": "/AI-CSS-Generator/static/js/main.4e31d655.chunk.js"
+  },
+  {
+    "revision": "f151a0a08337d8a08bf2",
+    "url": "/AI-CSS-Generator/static/js/runtime-main.94a345bf.js"
+  }
+]);
